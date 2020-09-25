a296f835028fe71ebbcd15700b36c7295e33e9ab
myhat123 <605162579@qq.com>
2020-09-25 15:03:01 +0800

    增加水印功能代码

--- hello_09/vcode.py
+++ hello_09/vcode.py
@@ -1,5 +1,6 @@
 import hashlib
 import random
+import math
 from datetime import datetime
 from io import BytesIO
 from PIL import Image, ImageDraw, ImageFont
@@ -42,6 +43,45 @@ def make_image(FONT: str, s: str) -> BytesIO:
 
     return buffer
 
+def center_text(text):
+    '''文本列表居中对齐'''
+
+    max_len = max([len(x.encode('gbk')) for x in text])
+
+    results = []
+    for t in text:
+        s = int(math.ceil((max_len - len(t.encode('gbk'))) / 2.0))
+        n = ' '*s + t + ' '*s
+        results.append(n)
+
+    return results
+
+def add_text_to_image(FONT, text):
+    '''加水印文字'''
+
+    font = ImageFont.truetype(FONT, 24)
+
+    w, h = (400, 360)
+
+    text_overlay = Image.new('RGBA', (w, h))
+    image_draw = ImageDraw.Draw(text_overlay)
+
+    x, y = w/3, h/3
+
+    #多行文字
+    for t in text:
+        image_draw.text((x, y), t, font=font, fill=(0, 0, 0, 30))
+        x = x
+        y = y + 28
+
+    #旋转
+    text_overlay = text_overlay.rotate(30)
+
+    buffer = BytesIO()
+    text_overlay.save(buffer, 'png')
+
+    return buffer
+
 if __name__ == '__main__':
     s = gen_rand_str()
     fontfile = '/usr/share/fonts/truetype/wqy/wqy-microhei.ttc'
@@ -49,3 +89,11 @@ if __name__ == '__main__':
     print(s)
     x = make_image(fontfile, s)
     print(x.getvalue())
+
+    text = ['10.239.1.1', '袁山大道营业所']
+    s = center_text(text)
+    print(s)
+    
+    y = add_text_to_image(fontfile, s)
+    print(y.getvalue())
+
