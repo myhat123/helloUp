52fc0d34e13c404673bdb11a44e560250c016be6
myhat123 <605162579@qq.com>
2020-10-04 10:32:17 +0800

    增加分组汇总

--- hello_03/README.md
+++ hello_03/README.md
@@ -9,7 +9,12 @@ cassandra配置，参见hello_07
 gradle build
 gradle shadowJar
 
+gradle :brchrpt:build
+gradle :brchrpt:shadowJar
+
 提交计算
 =======
 
 > spark-submit --master spark://localhost:7077 --class brchqry.BrchQry ./brchqry/build/libs/brchqry-all.jar
+
+> spark-submit --master spark://localhost:7077 --total-executor-cores 2 --executor-memory 512M --class brchrpt.BrchRpt ./brchrpt/build/libs/brchrpt-all.jar
---
+++ hello_03/brchrpt/src/main/scala/brchrpt/BrchRpt.scala
@@ -0,0 +1,38 @@
+package brchrpt
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+import conf.{Utils, CassSetting}
+
+object BrchRpt {
+  def main(args: Array[String]) {
+    val spark = SparkSession
+      .builder
+      .config("spark.cassandra.connection.host", CassSetting.host)
+      .config("spark.cassandra.auth.username", CassSetting.username)
+      .config("spark.cassandra.auth.password", CassSetting.password)
+      .appName("计算rpt_sum汇总")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    val df = spark
+        .read
+        .format("org.apache.spark.sql.cassandra")
+        .options(Map( "table" -> "brch_qry_dtl", "keyspace" -> "finance"))
+        .load
+
+    df.createOrReplaceTempView("qry_dtl")
+
+    spark.sql("""
+        select rpt_sum, sum(amt) from qry_dtl
+        where tran_date='2019-11-27'
+          and dr_cr_flag=1
+        group by rpt_sum
+    """).show()
+
+    spark.stop()
+  }
+}
